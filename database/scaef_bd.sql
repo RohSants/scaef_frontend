da6f54a4c0b5b3d89c2c3b2753e1d78dc42049f5
Roh <2019324007@ifam.edu.br>
2021-09-22 16:34:33 -0400

    implementação na tela de login e Banco de Dados
    * O Botão ainda deve ser editado e reajustado;
    
    A tela de login possui uso de bootstrap nos itens, exceto nos contêiners, pois fazem uso de de flex layout.

---
+++ database/scaef_bd.sql
@@ -0,0 +1,38 @@
+create table medicamento(
+	codigo int not null,
+    nome varchar(95) not null,
+    cid varchar(5) primary key,
+    mg int null,
+    ml int null
+);
+
+create table paciente (
+	Nome varchar(95) primary key,
+    CNS int Not Null,
+    CPF int Not Null,
+    DataNasc date Not Null,
+    Email varchar(100) Not Null,
+    Municipio varchar(26) Not Null,
+    Endereco varchar(100) Not Null,
+    Complemento varchar(50) Null,
+    CEP int Not Null,
+    Num int Not Null,
+    Tel int Null,
+    Fone int Null,
+    Unique(CPF)
+);
+
+create table usuarios(
+	Nome varchar(95) Not NULL,
+    CPF int Not NULL,
+    RG int Not NULL,
+    CNS int primary key,
+    Email varchar(100) NULL,
+    Celular int NULL,
+    Fone int NULL,
+    Funcao varchar(14) Not NULL,
+    CRF int NULL,
+    Unique(CPF),
+    Unique(RG),
+    Unique(CRF)
+);
