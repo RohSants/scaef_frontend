627f9d37c1b4ecee1d7f2fcc3d281283d40118af
MayraCarvalho15 <mayracarvalho.mrc@gmail.com>
2021-09-28 02:12:59 -0300

    alteraDTOeServ
    Inserções dos  atributos na class UsuarioDTO e no UsuarioService ainda falta finalizar

--- spring/src/main/java/com/scaef/spring/dto/UsuarioDTO.java
+++ spring/src/main/java/com/scaef/spring/dto/UsuarioDTO.java
@@ -1,12 +1,53 @@
 package com.scaef.spring.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
-    
+    @NotNull
+    private int id;
 
+    @NotBlank
+    @Size(max = 95)
+    private String nome;
+
+    @NotBlank
+    @Size(max = 11)
+    private String cpf;
+
+    @NotBlank
+    @Size(max = 9)
+    private String rg;
+    
+    @NotBlank
+    @Size(max = 95)
+    private String email;
+
+    @NotNull
+    private int celular;
+
+    @NotNull
+    private int fone;
+
+    @NotBlank
+    @Size(max = 20)
+    private String funcao;
+
+    @NotNull
+    private int crf;
+    
+    @Valid
+    @NotNull
+    private UsuarioDTO usuarioDTO;
     
 }
--- spring/src/main/java/com/scaef/spring/service/UsuarioService.java
+++ spring/src/main/java/com/scaef/spring/service/UsuarioService.java
@@ -1,5 +1,17 @@
 package com.scaef.spring.service;
 
+import com.scaef.spring.dto.MessageResponseDTO;
+import com.scaef.spring.dto.UsuarioDTO;
+import com.scaef.spring.entity.Usuario;
+import com.scaef.spring.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+    private UsuarioRepository usuarioRepository;
     
+    @Autowired
+	public UsuarioService(UsuarioRepository usuarioRepository){
+		this.usuarioRepository  = usuarioRepository;
 }
