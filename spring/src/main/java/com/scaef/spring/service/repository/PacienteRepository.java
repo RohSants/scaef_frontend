f6d6f7f1763bb987faf73668af283605264527cc
Roh <2019324007@ifam.edu.br>
2021-09-30 01:06:55 -0400

    Reconstrução do Repositório

--- spring/src/main/java/com/scaef/spring/service/repository/PacienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.scaef.spring.service.repository;
-
-import com.scaef.spring.entity.Paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PacienteRepository extends JpaRepository <Paciente, Integer> {
-    
-}
