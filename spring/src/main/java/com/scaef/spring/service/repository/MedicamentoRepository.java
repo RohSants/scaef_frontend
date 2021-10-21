80ddc9a9a891b0ae2158bb2c69b253020119db89
Roh <2019324007@ifam.edu.br>
2021-10-21 17:41:47 -0400

    ajustes no front-end para fundir a branch com a main

--- spring/src/main/java/com/scaef/spring/service/repository/MedicamentoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.scaef.spring.service.repository;
-
-import com.scaef.spring.entity.Medicamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicamentoRepository extends JpaRepository <Medicamento, Integer> {
-    
-}
--- spring/src/main/java/com/scaef/spring/service/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.scaef.spring.service.repository;
-
-import com.scaef.spring.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
-}
